6ff31ae74ef699e52bec6984c6af808bd9bee262
pete <work@pete-hamlin.com>
2022-12-03 19:09:46 +0000

    fix: Fixed delete/archive lambdas

--- src/linkding/__init__.py
+++ src/linkding/__init__.py
@@ -41,6 +41,7 @@ def show_articles(query) -> List[Item]:
     results = []
     for article in config.get_articles():
         article_id, title, article_url = article["id"], article["title"], article["url"]
+        link_id = article["id"]
         tags = ", ".join([tag for tag in article["tag_names"]])
         if filter_query(query.string, [title, tags, article_url]):
             debug(f"Got article: {title} - query string {query.string}")
@@ -56,8 +57,8 @@ def show_articles(query) -> List[Item]:
                         UrlAction(text="Open link in browser", url=article_url),
                         ClipAction(text="Copy link URL",
                                    clipboardText=article_url),
-                        FuncAction(text="Archive link", callable=lambda link_id=article_id: archive_link(link_id))
-                        FuncAction(text="Delete link", callable=lambda link_id=article_id: delete_link(link_id))
+                        FuncAction(text="Archive link", callable=lambda link_id=article_id: archive_link(link_id)),
+                        FuncAction(text="Delete link", callable=lambda link_id=article_id: delete_link(link_id)),
                     ],
                 )
             )
@@ -68,7 +69,8 @@ def show_articles(query) -> List[Item]:
                 icon=iconPath,
                 text="No articles found",
                 actions=[
-                    UrlAction(text="Open linkding", url=config.base_url)
+                    UrlAction(text="Open linkding", url=config.base_url),
+                    FuncAction(text="Refresh Articles", callable=config.refresh_articles),
                 ])),
     return results
 
@@ -81,20 +83,24 @@ def filter_query(query_string: str, filters: List[str]) -> bool:
 
 def delete_link(link_id: str):
     url = f"{config.base_url}/api/bookmarks/{link_id}"
+    debug("About to DELETE {}".format(url))
     response = requests.delete(url, headers=config.headers)
-    if not response.ok:
-        error("Got response {}".format(response))
+    if response.ok:
+        config.refresh_articles()
+    else:
+        warn("Got response {}".format(response))
 
 def archive_link(link_id: str):
     url = f"{config.base_url}/api/bookmarks/{link_id}/archive/"
     post_request(url)
 
 def post_request(url: str):
+    debug("About to POST {}".format(url))
     response = requests.post(url, headers=config.headers)
-    if response.ok:
+    if response.ok and response.content:
         return response.json()
     else:
-        error("Got response {}".format(response))
+        warn("Got response {}".format(response))
 
 class ApiConfig:
     def __init__(self, config) -> None:
@@ -112,10 +118,10 @@ class ApiConfig:
 
     def get_articles(self) -> List[Dict]:
         if self.article_expiry < datetime.now():
-            self._refresh_articles()
+            self.refresh_articles()
         return self.articles
 
-    def _refresh_articles(self):
+    def refresh_articles(self):
         url = f"{self.base_url}/api/bookmarks/?{self._get_params()}"
         debug("About to GET {}".format(url))
         articles = []
